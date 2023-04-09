552c190817933441eba85dac5522d4f1af88f6bd
Miro Zukina <92750661+MiroZukina@users.noreply.github.com>
2023-04-09 20:33:34 +0100

    Add files via upload

---
+++ class 2/task 1,2,3/task.js
@@ -0,0 +1,20 @@
+// task 1
+function f(x) {
+    return x*x - 5*x + 6;
+  }
+  console.log(f(5)); 
+
+//task 2
+
+function quotien(x,y){
+    return ~~(x/y);
+}
+console.log(quotien(9,4));
+
+//task 3
+
+function fahrenheitToCelsius(f) {
+    return 5 / 9 * (f-32);
+ }
+ console.log(fahrenheitToCelsius(68));
+
